e3bcbc77669719be7b8ba91a008078de7b4ac9d6
Tyler Scott <tascott@ucdavis.edu>
2024-10-28 11:46:48 -0700

    time series and facets

---
+++ 6_time_series_facets.R
@@ -0,0 +1,39 @@
+library(tidyverse)
+
+surveys_complete <- read_csv('data/portal_data_joined.csv') %>%
+  filter(complete.cases(.))
+
+# these are two different ways of doing the same thing
+head(surveys_complete %>% count(year,species_id))
+head(surveys_complete %>% group_by(year,species_id) %>% tally())
+
+yearly_counts <- surveys_complete %>% count(year,species_id)
+
+head(yearly_counts)
+
+ggplot(data = yearly_counts,mapping = aes(x = year, y = n)) +
+  geom_point()
+
+ggplot(data = yearly_counts,mapping = aes(x = year, y= n)) +
+  geom_line()
+
+ggplot(data = yearly_counts,mapping = aes(x = year, y= n,group = species_id)) +
+  geom_line()
+
+
+ggplot(data = yearly_counts,mapping = aes(x = year, y= n, colour = species_id)) +
+         geom_line()
+
+
+ggplot(data = yearly_counts, mapping = aes(x = year, y = n)) +
+  geom_line() +
+  facet_wrap(~ species_id)
+
+
+ggplot(data = yearly_counts, mapping = aes(x = year, y = n)) +
+  geom_line() +
+  facet_wrap(~ species_id,scales = 'free')
+
+
+
+       
