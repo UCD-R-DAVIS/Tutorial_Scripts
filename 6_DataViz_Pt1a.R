3ff1d9ac0039fa2a3c89b45d28ce232382c26141
Tara Pozzi <57369309+tarapozzi@users.noreply.github.com>
2024-10-31 12:48:11 -0700

    Add files via upload

---
+++ 6_DataViz_Pt1a.R
@@ -0,0 +1,55 @@
+# ggplot2 ----
+## Grammar of Graphics plotting package (included in tidyverse package - you can see this when you call library(tidyverse)!)
+## easy to use functions to produce pretty plots
+## ?ggplot2 will take you to the package helpfile where there is a link to the website: https://ggplot2.tidyverse.org - this is where you'll find the cheatsheet with visuals of what the package can do!
+
+## ggplot basics
+  ## every plot can be made from these three components: data, the coordinate system (ie what gets mapped), and the geoms (how to graphical represent the data)
+  ## Syntax: ggplot(data = <DATA>) + <GEOM_FUNCTION>(mapping = aes(<MAPPING>))
+
+## tips and tricks
+    ## think about the type of data and how many data  variables you are working with -- is it continuous, categorical, a combo of both? is it just one variable or three? this will help you settle on what type of geom you want to plot
+  ## order matters! ggplot works by layering each geom on top of the other
+  ## also aesthetics like color & size of text matters! make your plots accessible 
+
+
+## example ----
+library(tidyverse)
+## load in data
+surveys <- read_csv("data/portal_data_joined.csv") %>%
+  filter(complete.cases(.)) # remove all NA's
+
+## Let's look at two continuous variables: weight & hindfoot length
+## Specific geom settings
+
+
+## Universal geom settings
+
+
+
+
+
+
+
+
+## Visualize weight categories and the range of hindfoot lengths in each group
+## Bonus from hw: 
+sum_weight <- summary(surveys$weight)
+surveys_wt_cat <- surveys %>% 
+  mutate(weight_cat = case_when(
+    weight <= sum_weight[[2]] ~ "small", 
+    weight > sum_weight[[2]] & weight < sum_weight[[5]] ~ "medium",
+    weight >= sum_weight[[5]] ~ "large"
+  )) 
+
+table(surveys_wt_cat$weight_cat)
+
+
+## We have one categorical variable and one continuous variable - what type of plot is best?
+
+
+
+
+## What if I want to switch order of weight_cat? factor!
+
+
